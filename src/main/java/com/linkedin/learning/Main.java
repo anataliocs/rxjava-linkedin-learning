a236557f5c0f843ab0e56dec59c50c983f1219fc
Chris Anatalio <chris.anatalio@opengov.com>
2018-05-02 08:10:41 -0700

    Creating new observables

--- src/main/java/com/linkedin/learning/Main.java
+++ src/main/java/com/linkedin/learning/Main.java
@@ -1,16 +1,51 @@
 package com.linkedin.learning;
 
 
-import rx.Observable;
+import io.reactivex.Observable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    hello("Chris", "Jon Snow", "Ironman");
+    hello("My old friend", "Chris", "World");
+
+    //Create
+    String[] stringArray = new String[]{"hi", "hola", "bonjour"};
+    Observable<String> observable = Observable.fromArray(stringArray);
+
+    //
+    List<Integer> list = Arrays.asList(new Integer[]{1, 2, 3, 4});
+    Observable<Integer> integerObservable = Observable.fromIterable(list);
+
+    //
+    Observable<String> stringObservable = Observable.just("one object");
+
+
+    Observable.just("Hello");
+
+    Observable customObservableNonBlocking = customObservableNonBlocking();
   }
 
   public static void hello(String... names) {
-    Observable.from(names).subscribe(s -> System.out.println("Hello " + s + "!"));
+    Observable.fromArray(names).subscribe(s -> System.out.println("Hello " + s + "!"));
   }
+
+
+  public static Observable customObservableNonBlocking() {
+    return Observable.create(emitter -> {
+      try {
+        List<String> todos = new ArrayList<>();
+        for (String todo : todos) {
+          emitter.onNext(todo);
+        }
+        emitter.onComplete();
+      } catch (Exception e) {
+        emitter.onError(e);
+      }
+    });
+  }
+
 }
